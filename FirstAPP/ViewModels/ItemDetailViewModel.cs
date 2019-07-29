ee0d49374ccdeb611fd38cf2f0e14c092c16c08b
tangxian <tangxian@mpen.com.cn>
2019-07-29 10:34:29 +0800

    添加项目文件。

---
+++ FirstAPP/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FirstAPP.Models;
+
+namespace FirstAPP.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
